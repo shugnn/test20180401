3b91f0480395b00ee5d5fd5b48c307917ec7b3cf
shugnn-home <shugnn@163.com>
2018-04-07 23:22:25 +0800

    new commit

---
+++ server.js
@@ -0,0 +1,19 @@
+//var http = require("http");
+//http.createServer(function(request,response){
+//  response.writeHead(900,{"Content-Type":"text/plain"});
+//  response.write("Hello Worlds");
+//  response.end();
+//}).listen(8888);
+
+var http = require("http");
+
+function onRequest(request, response) {
+  console.log("Request received.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("OnResponse.");
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
--- testABC.js
+++ testABC.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+//function show(something)
+//{
+//  console.log(something);
+//}
+function excute(func,valu)
+{
+  func(valu);
+}
+excute(function(valu){console.log(valu)},"A Successful.")
